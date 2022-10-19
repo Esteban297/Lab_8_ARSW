c85837d7bba08e37667d46a9d6f3efb8e16a6e4e
Esteban297 <estebancristancho9@gmail.com>
2022-10-19 18:29:43 -0500

    Parte IV

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,24 +1,57 @@
 var app = (function () {
+    
     class Point{
         constructor(x,y){
             this.x=x;
             this.y=y;
         }        
     }
-    
+
     var stompClient = null;
-    var id = 0;
+    var idDrawing = null;
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    var input = document.querySelector('#idDrawing');
+
+    var loadEventListener = function(){
+        if( input ) input.addEventListener('change', updateId);
+        const eventCanvas = (window.PointerEvent)?'pointerdown':'mousedown';
+        canvas.addEventListener(eventCanvas, eventPoint);
+    }
+
+    var updateId = function(event){
+        idDrawing = event.target.value;
+        console.log(`nuevoValor ${idDrawing}`);
+        app.disconnect();
+    }
+
+    var eventPoint = function (event){
+        const { x ,y } = getMousePosition(event);
+        //publicar el event
+        if(idDrawing) app.publishPoint(x,y);
+    }
+
     var addPointToCanvas = function (point) {        
-        var canvas = document.getElementById("canvas");
-        var ctx = canvas.getContext("2d");
         ctx.beginPath();
-        ctx.arc(point.x, point.y, 1, 0, 2 * Math.PI);
+        ctx.arc(point.x, point.y, 3, 0, 2 * Math.PI);
         ctx.stroke();
     };
-    
-    
+
+    var drawPolygon = function(points){
+        canvas.width = canvas.width;
+        var { x: fstPosX, y: fstPosY } = points[0];
+        ctx.moveTo(fstPosX, fstPosY);
+
+        points.forEach(point => {
+            const { x , y } = point;
+            ctx.arc(x, y, 3, 0, 2 * Math.PI);
+            ctx.lineTo(x,y);
+        });
+        ctx.lineTo(fstPosX, fstPosY);
+        ctx.stroke();
+    }
+
     var getMousePosition = function (evt) {
-        canvas = document.getElementById("canvas");
         var rect = canvas.getBoundingClientRect();
         return {
             x: evt.clientX - rect.left,
@@ -33,36 +66,57 @@ var app = (function () {
         //subscribe to /topic/TOPICXX when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/newpoint' +id, function (eventbody) {
-                var theObject = JSON.parse(eventbody.body);
-                addPointToCanvas(theObject);
+            canvas.width = canvas.width;
+            stompClient.subscribe(`/topic/newpoint.${idDrawing}`, function (eventbody) {
+                const point = JSON.parse(eventbody.body);
+                addPointToCanvas(point);
             });
+
+            stompClient.subscribe(`/topic/newpolygon.${idDrawing}`, function (eventbody) {
+                const points = JSON.parse(eventbody.body);
+                drawPolygon(points);
+            });
+            stompClient.subscribe(`/topic/queue.${idDrawing}`, function (eventbody) {
+                const points = JSON.parse(eventbody.body);
+                if( points.length  < 4 ){
+                    points.forEach(point => addPointToCanvas(point));
+                    return;
+                }
+                drawPolygon(points);
+            });
+            stompClient.send(`/app/queue.${idDrawing}`, {});
         });
     };
-    
-    
+        
+   
+
+
     return {
 
         init: function () {
-            var can = document.getElementById("canvas");
-            id = $('#id'.val());
-            //websocket connection
-            connectAndSubscribe();
+            loadEventListener();
         },
+
         publishPoint: function(px,py){
             var pt=new Point(px,py);
             console.info("publishing point at "+pt);
             addPointToCanvas(pt);
-            stompClient.send("/topic/newpoint"+id, {}, JSON.stringify(pt));
             //publicar el evento
+            stompClient.send(`/app/newpoint.${idDrawing}`, {}, JSON.stringify(pt));
         },
 
         disconnect: function () {
             if (stompClient !== null) {
                 stompClient.disconnect();
             }
-            setConnected(false);
+            //setConnected(false);
             console.log("Disconnected");
+        },
+        publishDrawing( currentId ){
+            idDrawing = currentId;
+            //websocket connection
+            connectAndSubscribe();
         }
     };
+
 })();
